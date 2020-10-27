416a62f903843b16b45cf5d054ac3481c8ec507d
M Colliver <m.colliver@warwickschool.org>
2020-10-27 21:11:03 +0000

    Update Snail_attempt.py

--- Snail_attempt.py
+++ Snail_attempt.py
@@ -18,3 +18,30 @@ def snail(snail_map):
         overall.append(final[-2::-1])
         print(overall)
     pass
+ 
+    
+ def snail(snail_map):
+   
+    length = len(snail_map)
+    print('SN',snail_map,'length',length)
+    if len(snail_map)>1 :
+        snaill = snail_map[0]
+        snail_map.pop(0)
+        for i in range(1,length-1):
+            snaill.append( snail_map[i-1][length-1])
+            snail_map[i-1].pop(length-1)
+        print(snail_map)
+        for i in range(length-1,-1,-1):
+            print(length-2,i)
+            snaill.append( snail_map[length-2][i])
+        snail_map.pop(length-2)
+        for i in range(length-2,1,-1):
+            snaill.append( snail_map[i-1][0])
+            snail_map[i-1].pop(0)
+            
+        return snaill + snail(snail_map)
+    elif len(snail_map)==1:
+        return snail_map[0]
+    else:
+        return [] 
+        
